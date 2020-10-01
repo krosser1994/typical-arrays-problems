4a471089853e5d9c06cebb94737f15222c68905e
Yahor Kasykh <70840812+krosser1994@users.noreply.github.com>
2020-10-01 23:01:53 +0300

    feat: add test solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  } else {
+      let minNumber = array[0];
+      for (let i = 1; i < array.length; i++){
+        if (array[i] < minNumber){
+          minNumber = array[i];
+        } 
+      }
+      return minNumber;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+ if (array === undefined || array.length === 0) {
+    return 0;
+  } else {
+      let maxNumber = array[0];
+      for (let i = 1; i < array.length; i++) {
+        if (array[i] > maxNumber) {
+          maxNumber = array[i];
+        }
+      }
+      return maxNumber;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum = 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  } else {
+      let count = array.length;
+      for (let i = 0; i < array.length; i++) {
+        sum = sum + array[i];
+      }
+      let average = sum / count;
+      return average;
+    }
 }
